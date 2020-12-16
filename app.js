a4e5d655d72b4f597b20f517e93cf5d0fec3f611
Carla <ccairns1999@gmail.com>
2020-12-16 15:40:39 -0600

    rainbow button and style added. rainbow functionality works on its own but not together with default

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 let container = document.getElementById('container');
 let wrapper = document.getElementById('wrapper').appendChild(container);
 let grid = 16;
+let rainbowClick = false;
+
+let clearBtn = document.createElement("button");
+clearBtn.title = "Clear";
+clearBtn.innerHTML = "Clear";
+clearBtn.id = "clear";
+document.getElementById("modifications").appendChild(clearBtn);
+
+clearBtn.addEventListener("click", fucntion=>{
+    blocks.forEach(block =>{
+        block.classList.remove("hover");
+    });
+});
+//adding rainbow button
+let rainbow = document.createElement("button");
+rainbow.title = "Rainbow color";
+rainbow.innerHTML = "Rainbow";
+rainbow.id = "rainbow";
+document.getElementById("modifications").appendChild(rainbow);
+
+
+//creating grid
 for (let i = 0; i < grid; i++ ){
     for (let j = 0; j < grid; j++){
     let gridDiv = document.createElement("div");
@@ -15,24 +37,36 @@ for (let i = 0; i < nodeBlocks.length; i++){
     //must be array, thus for loop conversion
     blocks.push(nodeBlocks[i]);
 }
-blocks.forEach(block => {
+rainbow.addEventListener("click", fucntion=>{
+    rainbowClick = true;    
+   
+});
+
+function draw(){
+//default coloring action when mousing over
+    console.log(rainbowClick);
     //array necessary
-    block.addEventListener("mouseover", mouseOver=>{
-        block.classList.add("hover"); 
-        //adding hover class if mouse is on block
-        // to leave a pixelated trail
+    
+        blocks.forEach(block => {
+            if (rainbowClick){
+            //console.log("rgb("+rgb.join(",")+")");
+            block.addEventListener("mouseover", mouseOver =>{
+                let rgb = [Math.floor(Math.random() *256), Math.floor(Math.random()*256), Math.floor(Math.random()*256)]
+                //rbg array with 3 random numbers up to 256
+                block.style.backgroundColor = "rgb("+rgb.join(",")+")";
+            });
+            }
+            else {
         
+                block.addEventListener("mouseover", mouseOver=>{
+                block.classList.add("hover"); 
+                //adding hover class if mouse is on block
+                // to leave a pixelated trail
+        
+                });
+            }
     });
-});
 
-let clearBtn = document.createElement("button");
-clearBtn.title = "Clear";
-clearBtn.innerHTML = "Clear";
-clearBtn.id = "clear";
-document.getElementById("modifications").appendChild(clearBtn);
+}
 
-clearBtn.addEventListener("click", fucntion=>{
-    blocks.forEach(block =>{
-        block.classList.remove("hover");
-    });
-});
+draw();
