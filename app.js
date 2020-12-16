32a9282192bfdd654ddc272d1a0e05709c3480f3
Carla <ccairns1999@gmail.com>
2020-12-16 13:26:00 -0600

    some style added and clear button implemented

--- app.js
+++ app.js
@@ -12,19 +12,27 @@ for (let i = 0; i < grid; i++ ){
 let nodeBlocks = document.querySelectorAll(".block");
 let blocks = [];
 for (let i = 0; i < nodeBlocks.length; i++){
+    //must be array, thus for loop conversion
     blocks.push(nodeBlocks[i]);
 }
 blocks.forEach(block => {
-    //must be array, thus for loop conversion
+    //array necessary
     block.addEventListener("mouseover", mouseOver=>{
         block.classList.add("hover"); 
         //adding hover class if mouse is on block
         // to leave a pixelated trail
         
     });
-   
-
 });
 
+let clearBtn = document.createElement("button");
+clearBtn.title = "Clear";
+clearBtn.innerHTML = "Clear";
+clearBtn.id = "clear";
+document.getElementById("modifications").appendChild(clearBtn);
 
-
+clearBtn.addEventListener("click", fucntion=>{
+    blocks.forEach(block =>{
+        block.classList.remove("hover");
+    });
+});
