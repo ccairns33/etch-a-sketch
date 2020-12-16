44738f7e74e4b003011300dfa5d2cbd47ebcb3d9
Carla <ccairns1999@gmail.com>
2020-12-15 21:54:57 -0600

    pixelated trail when mouse hovers over grid

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ for (let i = 0; i < grid; i++ ){
     gridDiv.id = "block";
     gridDiv.className = "block";
     document.getElementById('container').appendChild(gridDiv);
-        console.log()
     }
 }
+let nodeBlocks = document.querySelectorAll(".block");
+let blocks = [];
+for (let i = 0; i < nodeBlocks.length; i++){
+    blocks.push(nodeBlocks[i]);
+}
+blocks.forEach(block => {
+    //must be array, hus for loop conversion
+    block.addEventListener("mouseover", mouseOver=>{
+        block.classList.add("hover"); 
+        //adding hover class if mouse is on block
+        // to leave a pixelated trail
+        
+    });
+   
+
+});
+
+
+
